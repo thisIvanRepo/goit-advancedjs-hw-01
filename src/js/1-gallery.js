90dd41933dc07e478a4e6e5a56f8c0abd40770b5
Ivan_Enko <110199ws@gmail.com>
2025-03-11 16:24:43 +0200

    firs task done

---
+++ src/js/1-gallery.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import images from './img.js';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+      return;
+  }
+});
+
+const markupItemsImages = images
+    .map((image) => getHtmlLiImages(image))
+    .join('');
+
+gallery.innerHTML = markupItemsImages;
+
+new SimpleLightbox('.gallery-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  
+});
+
+function getHtmlLiImages(image) {
+    const { preview, original, description } = image;
+
+  return `
+          <li class="gallery-item">
+	          <a class="gallery-link" href="${original}">
+		          <img
+			          class="gallery-image"
+			          src="${preview}"
+			          alt="${description}"
+			        />
+	          </a>
+          </li>`;
+}
+
